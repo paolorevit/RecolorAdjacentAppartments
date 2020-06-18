d42750c4df03f8b46bf13a85391a7dc633327146
Pavel <paolo_07@list.ru>
2020-06-18 12:08:00 +0500

    Empty project

---
+++ RecolorAdjacentAppartments/RecolorAdjacentAppartments/Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+
+namespace RecolorAdjacentAppartments
+{
+     [Autodesk.Revit.Attributes.Transaction(Autodesk.Revit.Attributes.TransactionMode.Manual)]
+    public class Class1:IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            UIApplication uiapp = commandData.Application;
+            UIDocument uidoc= uiapp.ActiveUIDocument;
+            Document doc = uidoc.Document;
+            return Result.Succeeded;
+        }
+    }
+}
